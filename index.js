c2ef299c6e5a82b87a9c473604b2e1e6de7bcce9
Patrick marquezini <81092778+patrick1928@users.noreply.github.com>
2023-02-01 21:06:55 -0300

    adicionando mais operadores

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 function calcular(){
    var entrada = document.getElementById('entrada')
    var res = document.getElementById('res')
-   res.innerHTML = '<p>Tabuada de:<p/>'
-   if (entrada.value.length == 0 || Number(entrada.value) < 1){
-    window.alert('[ERROR]- por favor digite uma tabuada valida')
-   }
-   else{
-    var t = Number(entrada.value)
-    var i = 0
+   var select = document.getElementById('select')
+   let t = Number(entrada.value)
+    let i = 0
     let r = 0
     res.innerText += `${t}`
+   res.innerHTML = '<p>Tabuada de:<p/>'
+   if (entrada.value.length == 0){
+    window.alert('[ERROR]- por favor digite uma tabuada valida')
+   }
+   //adição
+   else if (select.value == 'adição'){
     do{
-       
-        r = t * i
-        res.innerHTML += `<br>${t} * ${i} = ${r} `
+        r = t + i
+        res.innerHTML += `<br>${t} + ${i} = ${r} `
         i++
         
     }while(i <= 10)
    }
+   //subtração
+   else if (select.value == 'subtração'){
+    for(i ;i <= 10;i++){
+        r = t - i
+        res.innerHTML += `<br>${t} - ${i} = ${r}`
+    }
+    }
+    //multiplicação
+    else if (select.value == 'multiplicação'){
+        for(i ;i <= 10;i++){
+            r = t * i
+            res.innerHTML += `<br>${t} x ${i} = ${r}`
+        }
+    }
+    //divisão
+    else{
+        for(i ;i <= 10;i++){
+            r = t / i
+            res.innerHTML += `<br>${t} / ${i} = ${r}`
+        }
+    }
+   
 }
